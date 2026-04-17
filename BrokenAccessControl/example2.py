49d7f2443acd15558b4467977da5038788e00c35
KillianW-IvyTech <kwallace120@ivytech.edu>
2026-04-17 11:44:41 -0400

    finished brokenaccesscontrol and made cryptographicfailures also readme edits

---
+++ BrokenAccessControl/example2.py
@@ -0,0 +1,20 @@
+'''
+@app.route('/account/<user_id>')
+def get_account(user_id):
+    user = db.query(User).filter_by(id=user_id).first()
+    return jsonify(user.to_dict())
+'''
+from flask import jsonify, abort
+from flask_login import login_required, current_user
+
+@app.route('/account/<int:user_id>')   # enforces type
+@login_required                         # rejects unauthenticated requests
+def get_account(user_id):
+    # authorization
+    # admins could be allowed here
+    if current_user.id != user_id:
+        abort(403)                       
+    user = db.query(User).filter_by(id=user_id).first()
+    if user is None:
+        abort(404)                       # safe
+    return jsonify(user.to_dict()), 200
